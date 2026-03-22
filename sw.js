826c89d9ffe9b2abc4a60854cdc262a20cff7fbd
ALHMS <maruyama.shotaro@alhinc.jp>
2026-03-23 00:33:19 +0900

    拡張選択追加

--- sw.js
+++ sw.js
@@ -1,9 +1,16 @@
-// 1. バージョン名を変更する（アップデートのたびに v2, v3... と変えていきます）
-const CACHE_NAME = 'memo-app-v4';
+const CACHE_NAME = 'memo-app-v5';
+
+// オフライン動作用に保存するファイルのリスト（新アイコン追加）
 const urlsToCache = [
   './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  './assets/undo.svg',
+  './assets/redo.svg',
+  './assets/trash.svg',
+  './assets/copy.svg',
+  './assets/paste.svg',
+  './assets/partial-select.svg'
 ];
 
 // インストール時にファイルをキャッシュする
@@ -14,17 +21,15 @@ self.addEventListener('install', event => {
         return cache.addAll(urlsToCache);
       })
   );
-  // 新しいService Workerを強制的に待機状態から実行状態にする
   self.skipWaiting(); 
 });
 
-// 2. 新しいService Workerが有効になったときに、古いキャッシュを削除する
+// 新しいService Workerが有効になったときに、古いキャッシュを削除する
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
-          // 現在のバージョン（v2）以外のキャッシュ（v1など）をすべて削除
           if (cacheName !== CACHE_NAME) {
             console.log('古いキャッシュを削除しました:', cacheName);
             return caches.delete(cacheName);
@@ -33,7 +38,6 @@ self.addEventListener('activate', event => {
       );
     })
   );
-  // コントロールを即座に新しいService Workerに移す
   self.clients.claim();
 });
 
